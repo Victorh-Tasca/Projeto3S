14e7dad23d4e88705920f7a56a085097f0471352
Victor Hugo da Silva Tasca <tascavictorhugo@gmail.com>
2020-09-18 16:41:22 -0300

    Criação e modificação de páginas
    Criação das páginas de esqueci minha senha, registro_3 e modificação da página de serviços do cliente

--- public_html/js/js.js
+++ public_html/js/js.js
@@ -7,27 +7,24 @@ function check_pass() {
     }
 }
 
-function habilitacao1() {
-    if (document.getElementById('user_reg').checked === true) {
-        document.getElementById('user_input').disabled = false;
-    }
-    if (document.getElementById('user_reg').checked === false) {
-        document.getElementById('user_input').disabled = true;
-    }
-}
 function habilitacao2() {
     if (document.getElementById('prest_reg').checked === true) {
         document.getElementById('prest_input').disabled = false;
     }
     if (document.getElementById('prest_reg').checked === false) {
         document.getElementById('prest_input').disabled = true;
+        document.getElementById('prest_input').value = '';
     }
 }
-function habilitacao3() {
-    if (document.getElementById('admin_reg').checked === true) {
-        document.getElementById('admin_input').disabled = false;
-    }
-    if (document.getElementById('admin_reg').checked === false) {
-        document.getElementById('admin_input').disabled = true;
+
+function check_newpass() {
+    if (document.getElementById('NewPassword').value ===
+            document.getElementById('ConfirmNewPassword').value) {
+        document.getElementById('enviar').disabled = false;
+    } else {
+        document.getElementById('enviar').disabled = true;
     }
 }
+function NewPassAlert() {
+    alert ("Senha alterada com sucesso!");
+  }
