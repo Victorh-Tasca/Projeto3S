8ff1a1cdc870e8f05d516baa24efdd6f4dd6704f
Victor Hugo da Silva Tasca <tascavictorhugo@gmail.com>
2020-09-18 02:31:36 -0300

    Criação e modificação de páginas
    criação da página 3 de registro e modificação da página inicial pós login

--- public_html/js/js.js
+++ public_html/js/js.js
@@ -6,3 +6,28 @@ function check_pass() {
         document.getElementById('registrar').disabled = true;
     }
 }
+
+function habilitacao1() {
+    if (document.getElementById('user_reg').checked === true) {
+        document.getElementById('user_input').disabled = false;
+    }
+    if (document.getElementById('user_reg').checked === false) {
+        document.getElementById('user_input').disabled = true;
+    }
+}
+function habilitacao2() {
+    if (document.getElementById('prest_reg').checked === true) {
+        document.getElementById('prest_input').disabled = false;
+    }
+    if (document.getElementById('prest_reg').checked === false) {
+        document.getElementById('prest_input').disabled = true;
+    }
+}
+function habilitacao3() {
+    if (document.getElementById('admin_reg').checked === true) {
+        document.getElementById('admin_input').disabled = false;
+    }
+    if (document.getElementById('admin_reg').checked === false) {
+        document.getElementById('admin_input').disabled = true;
+    }
+}
